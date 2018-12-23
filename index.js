155f8334cc50794186a1567fe8e061c98766f825
jgcc97 <gabrielcafaro@gmail.com>
2018-12-23 12:34:44 -0400

    Fix connection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
- 
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('index.html' , { root : __dirname});
 });
  
 http.listen(3000, function(){
